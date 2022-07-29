51e8c04ffbd4192d3dd1d892059d85e85735ff13
KampfKiwi08 <98646904+KampfKiwi08@users.noreply.github.com>
2022-07-29 21:33:00 +0200

    new project

---
+++ src/main/java/net/kiwi/tutorialmod/TutorialModClient.java
@@ -0,0 +1,11 @@
+package net.kiwi.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
